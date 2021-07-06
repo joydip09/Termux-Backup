06e7548db52f46a84cd3262cd7431f98748151c6
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-06 15:39:56 +0600

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,7 +1,25 @@
 #!bin/bash
 
+apt install ruby
+gem install lolcat
 termux-setup-storage
-cd /data/data/com.termux/files
-tar -zcf /sdcard/termux-backup.tar.gz home usr
-echo "Your Termux backup was created successfully in your internal storage" | lolcat -as 50
-echo "For restoring commands visit README.md" | lolcat -as 50
+
+if [ -a /sdcard/termux-backup.tar.gz ];
+then
+	echo "backup exists"
+else
+	echo "There is no backup in your device. Do u wanna make one?(y/n): "
+	read cnfrm
+fi
+
+if [ "$cnfrm" = "y" ];
+then
+	echo "Backup is being created" | lolcat -as 10
+	cd /data/data/com.termux/files
+	tar -zcf /sdcard/termux-backup.tar.gz home usr
+	echo "Your Termux backup was created successfully in your internal storage" | lolcat -as 50
+	echo "For restoring commands visit README.md" | lolcat -as 50
+	echo "Thanks for using my tool" | lolcat -as 10
+else
+	echo "Backup creating proccess is terminated" | lolcat -as 10
+fi

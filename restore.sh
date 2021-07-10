7df84f2811e446e0919b48dcc195d9bac8660a18
C_Joydip <75995283+C-Joydip@users.noreply.github.com>
2021-07-10 14:05:50 +0600

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -11,19 +11,24 @@ echo "For this proccess keep your 'termux-backup.tar.gz' file in main internal s
 
 if [ -a /sdcard/termux-backup.tar.gz ];
 then
-	echo "There is a backup in your device. Do u wanna restore?(y/n): "
+	echo "There is a backup in your device. Do u wanna restore?(y/n): " | lolcat -as 10
 	read cnfrm1
 else
 	echo "There is no backup in your device."
 fi
 
-if [ "$cnfrm1" = "y" ];
-then
-	echo "Backup is being restored" | lolcat -as 10
-	cd /data/data/com.termux/files
-	tar -zxf /sdcard/termux-backup.tar.gz --recursive-unlink --preserve-permissions
-	echo "If you got any error ignore them and restart your Termux" | lolcat -as 10
-	echo "Thanks For Using My Tool" | lolcat -as 10
-else
-	echo "Restoring creating proccess is terminated" | lolcat -as 10
-fi
+case "$cnfrm1" in
+	[yY] | [yY][eE][sS])
+		echo "Backup is being restored" | lolcat -as 10
+		cd /data/data/com.termux/files
+		tar -zxf /sdcard/termux-backup.tar.gz --recursive-unlink --preserve-permissions
+		echo "If you have got any error ignore them and restart your Termux" | lolcat -as 10
+		echo "Thanks For Using My Tool" | lolcat -as 10
+		;;
+	[nN] | [nN][oO])
+		echo "Restoring creating proccess is terminated" | lolcat -as 10
+		;;
+	*)
+		echo "Invalid option"
+		;;
+esac
